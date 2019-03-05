3da089dda496dd63df9503ecd1fcf29d7b428ef1
hryniewiczm <hryniewicz.m@o2.pl>
2019-03-05 18:12:39 +0100

    adding filtering to search page

---
+++ listings/choices.py
@@ -0,0 +1,24 @@
+bedroom_choices = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10
+}
+
+price_choices = {
+    '100000': 'PLN100,000',
+    '200000': 'PLN200,000',
+    '300000': 'PLN300,000',
+}
+
+state_choices = {
+    'MAŁOP': 'Małopolskie',
+    'POM': 'Pomorskie',
+    'ZACHPOM': 'Zachodniopomorskie',
+}
